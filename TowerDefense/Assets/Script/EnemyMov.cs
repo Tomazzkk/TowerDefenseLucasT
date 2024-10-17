e02f43d8d6823017a9a1ebf18a1821f4ead1f205
Tomazzkk <lucasttavares21@gmail.com>
2024-10-17 09:29:01 -0300

    Mov && Spawn do obj "Enemy" funcionando

--- TowerDefense/Assets/Script/EnemyMov.cs
+++ TowerDefense/Assets/Script/EnemyMov.cs
@@ -24,6 +24,7 @@ public class EnemyMov : MonoBehaviour
 
             if (pathIndex >= LevelManager.main.path.Length)
             {
+                EnemySpawner.onEnemyDestroy.Invoke();
                 Destroy(gameObject);
                 return;
             }
